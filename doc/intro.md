981064e9947339390f477bebdda7290d2f9c811e
Alexey Fedorov <waterlink000@gmail.com>
2015-03-13 21:04:04 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-followers
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_followers/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-followers.core-test
+  (:require [clojure.test :refer :all]
+            [clj-followers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
